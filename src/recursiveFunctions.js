b5be134084db704c3d043301c619fa846cda4f33
ViolaCello <ViolaCelloNYC@gmail.com>
2021-02-08 14:20:27 -0500

    recursiveSum function

--- src/recursiveFunctions.js
+++ src/recursiveFunctions.js
@@ -1,1 +1,14 @@
 onScreen("This is in Recursive")
+
+function recursiveSum(n) {
+    if (n <= 1) {
+        return n
+    } else {
+        onScreen("Inside recusiveSum, n=")
+        onScreen(n)
+        return (n + recursiveSum(n-1))
+    }
+}
+
+let ex = recursiveSum(11)
+onScreen(ex)
