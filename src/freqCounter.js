fcd535f8f0696f838a5c4d8823cfa224f926d1ba
ViolaCello <ViolaCelloNYC@gmail.com>
2021-02-19 14:56:47 -0500

    count the letters of each string

--- src/freqCounter.js
+++ src/freqCounter.js
@@ -2,4 +2,12 @@ function validAnagram(str1, str2){
     if (str1.length != str2.length) {
         return false
     }
+    let freq1 = {}
+    let freq2 = {}
+    for (letter of str1) {
+        freq1[letter] = freq1[letter] +1 || 1
+    }
+    for (letter of str2) {
+        freq1[letter] = freq1[letter] +1 || 1
+    }
   }
