5a1345b9cb89684f2ce2ca8b84e9c448b999cda8
ViolaCello <ViolaCelloNYC@gmail.com>
2021-02-23 16:26:23 -0500

    sum_zero in Ruby working correctly

--- multiple_pointer.rb
+++ multiple_pointer.rb
@@ -5,10 +5,11 @@
 #  return the index points of the first pair that = 0
 def sum_zero(arr)
     left = 0
-    right = arr.length -1
+    right = arr.length
+    right -=1
     while left<right do
-        if arr[left] + arr[right] == 0
-            return [left, right]
+         if arr[left] + arr[right] == 0
+            return left, right
         elsif arr[left] + arr[right] > 0
             right-= 1
         else 
