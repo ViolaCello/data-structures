c328a6217778dbc02669a9c6edacaa1b34eae714
ViolaCello <ViolaCelloNYC@gmail.com>
2021-02-25 14:31:02 -0500

    sameFrequency in JavaScript - attempt 1

--- src/multiplePointers.js
+++ src/multiplePointers.js
@@ -54,6 +54,27 @@ function sameFrequency(a,b){
     if (aa.length != bb.length) {
         return false
     }
+    if (aa===bb) {
+        return true
+    }
+    let freq1 = {}
+    let freq2 = {}
+    for (letter of str1) {
+        freq1[letter] = freq1[letter] +1 || 1
+    }
+    for (letter of str2) {
+        freq2[letter] = freq2[letter] +1 || 1
+    }
+    for (key in freq1) {
+        if (!(key in freq2)) {
+            return false
+        }
+        if (freq1[key] != freq2[key])
+        return false
+    }
+    console.log(freq1, freq2)
+    return true
     
   }
+  
   
