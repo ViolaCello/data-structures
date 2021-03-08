10d1d238af24f5919e777f1d791ff3f2b57ef4e3
ViolaCello <ViolaCelloNYC@gmail.com>
2021-03-08 16:19:43 -0500

    #running_sum using Ruby .each_with_index

---
+++ src/ruby_examples.rb
@@ -0,0 +1,14 @@
+# Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+# Return the running sum of nums.
+
+
+def running_sum(nums)
+    result = []
+     result[0] = nums[0]
+   nums.each_with_index do |n, i|
+       if i!=0
+           result.push(n + result[i-1])
+       end
+   end
+    return result
+end
