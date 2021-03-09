013bdfe585a914d86559750ef97e51a605e76b03
ViolaCello <ViolaCelloNYC@gmail.com>
2021-03-09 14:50:10 -0500

    is_Palindrome recursively in Ruby - returns true/false properly

--- src/ruby_recursive.rb
+++ src/ruby_recursive.rb
@@ -1,11 +1,15 @@
 def is_Palindrome(word)
     if word.length <= 1
+        puts true
         return true
     end
     if word[0] != word[-1]
+        puts false
         return false
-    end
+    else 
     word.slice!(-1)
-    word.slice(0)
-    is_Palindrome(word)
+    word.slice!(0)
+    puts word
+    return is_Palindrome(word)
+    end
 end
