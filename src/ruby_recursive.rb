def98c73590b4e20a07ccc185c32de595bfef31b
ViolaCello <ViolaCelloNYC@gmail.com>
2021-03-09 14:39:12 -0500

    is_Palindrome recursively in Ruby - first attempt

--- src/ruby_recursive.rb
+++ src/ruby_recursive.rb
@@ -2,9 +2,10 @@ def is_Palindrome(word)
     if word.length <= 1
         return true
     end
-    top = word[0]
-    last = word[-1]
-    puts top
-    puts last
-    puts word
+    if word[0] != word[-1]
+        return false
+    end
+    word.slice!(-1)
+    word.slice(0)
+    is_Palindrome(word)
 end
