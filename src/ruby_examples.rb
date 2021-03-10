02d2cb6aebf74a9ab78f81d199e58884f5227bc1
ViolaCello <ViolaCelloNYC@gmail.com>
2021-03-10 16:33:05 -0500

    binary search JavaScript - correct

--- src/ruby_examples.rb
+++ src/ruby_examples.rb
@@ -23,3 +23,48 @@ def linear_search(arr, item)
     end
    return -1
 end
+
+# binary search
+def binary_search(arr, value)
+    left = 0
+    right = arr.length - 1
+    mid = ((left+right)/2).floor
+    if arr[mid]==value 
+        puts mid
+        return mid
+    end
+    puts "line 10"
+    while arr[mid]!=value && left<=right do
+    mid = ((left+right)/2).floor
+       puts "line 12"
+       puts "left", left
+       puts "right", right
+       puts "mid", mid
+        if left==right-1
+            if arr[left]==value
+                puts "The result is,",left
+                return left
+            elsif  
+                arr[right]==value
+                puts "The result is, ",right
+                return right
+            else 
+                puts -1
+                return -1
+            end
+puts "line 28"
+        end
+        puts "line 30"
+        if arr[mid]==value
+            puts "The result is=", mid
+            return mid
+        elsif  
+            value<arr[mid]
+            right = mid
+        else 
+            left=mid
+        end
+    end
+    puts "last line, -1"
+    return -1
+end
