646f53b61980b12ce5c295142eedd1e3c8dbb01c
ViolaCello <ViolaCelloNYC@gmail.com>
2021-03-16 16:10:11 -0400

    insertionSort - JavaScript

--- src/sortingAlgos.js
+++ src/sortingAlgos.js
@@ -61,3 +61,17 @@ function selectionSort(arr) {
     }
     return arr
 }
+
+function insertionSort(arr) {
+    for (let i = 0; i<arr.length; i++) {
+      let currentValue = arr[i]
+       for (let j = i-1; j>=0 && arr[j] > currentValue; j--) {
+        arr[j+1] = arr[j]
+       
+       arr[j] = currentValue
+       }
+    }
+    return arr
+}
+
+// [4,3,9,6]
