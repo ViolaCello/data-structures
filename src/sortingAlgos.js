4dcd2e6b46db205df381480749137dc643b26ad0
ViolaCello <ViolaCelloNYC@gmail.com>
2021-03-17 14:56:10 -0400

    quickSort, with new pivot() helper function

--- src/sortingAlgos.js
+++ src/sortingAlgos.js
@@ -98,19 +98,38 @@ function mergeSort(arr) {
     return mergeSorted(left, right)
 }
 
-// return index of where first item would be sorted to
-function pivot(arr, start = 0, end = arr.length-1) {
-    let pivot = arr[start]
-    let pivotIndex = 0
-    for (let i = 1; i<= end; i++) {
-        if (pivot>arr[i]) {
-            pivotIndex++
-            let temp = arr[i]
-            arr[i] = arr[pivotIndex]
-            arr[pivotIndex] = temp
-        }
+// return index of where first item would be sorted to, helper to quickSort()
+
+function pivot(arr, start = 0, end = arr.length - 1) {
+    const swap = (arr, idx1, idx2) => {
+      [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    };
+  
+    // We are assuming the pivot is always the first element
+    let pivot = arr[start];
+    let swapIdx = start;
+  
+    for (let i = start + 1; i <= end; i++) {
+      if (pivot > arr[i]) {
+        swapIdx++;
+        swap(arr, swapIdx, i);
+      }
     }
-    arr[0] = arr[pivotIndex]
-            arr[pivotIndex] = pivot 
-    return pivotIndex
-}
+  
+    // Swap the pivot from the start the swapPoint
+    swap(arr, start, swapIdx);
+    return swapIdx;
+  }
+  
+  
+  function quickSort(arr, left = 0, right = arr.length -1){
+      if(left < right){
+          let pivotIndex = pivot(arr, left, right) //3
+          //left
+          quickSort(arr,left,pivotIndex-1);
+          //right
+          quickSort(arr,pivotIndex+1,right);
+        }
+       return arr;
+  } 
+             
