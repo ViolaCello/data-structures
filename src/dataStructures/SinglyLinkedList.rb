07aa7a9dc5573b35b85d54d57b3d69ce0cbd90cc
ViolaCello <ViolaCelloNYC@gmail.com>
2021-03-22 17:20:40 -0400

    Singly Linked Lists in Ruby.  #push instance method

--- src/dataStructures/SinglyLinkedList.rb
+++ src/dataStructures/SinglyLinkedList.rb
@@ -1,7 +1,30 @@
 class Node
-    attr_accessor: val
-    def initalize(val)
-        @val = val  
-    end
+    attr_accessor :val, :next
+   def initialize(val)
+       @val = val  
+       @next = nil
+   end
 end
 
+class SinglyLinkedList
+   attr_accessor :head, :tail, :length
+
+   def initialize()
+       @head = nil
+       @tail = nil
+       @length = 0
+   end
+
+   def push(val)
+       new_node = Node.new (val)
+       if !@head 
+           @head = new_node
+           @tail = new_node
+       else 
+           @tail.next = new_node
+           @tail = new_node
+       end
+       @length += 1
+       return self 
+   end
+end
