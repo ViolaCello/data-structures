56b0450600c05f357d16136a3b457c93cd17b0f5
ViolaCello <ViolaCelloNYC@gmail.com>
2021-03-27 14:55:30 -0400

    DoublyLinkedList push()

--- src/dataStructures/doublyLinkedList.js
+++ src/dataStructures/doublyLinkedList.js
@@ -12,4 +12,22 @@ class DoublyLinkedList {
         this.tail = null
         this.length = 0
     }
+
+    push(val) {
+        if (this.length===0) {
+            let newNode = new Node(val)
+            this.head = newNode
+            this.tail = newNode
+            this.length = 1
+        } else {
+            let newNode = new Node(val)
+            let temp = this.tail
+            temp.next = newNode
+            newNode.prev = temp
+            this.tail = newNode
+            this.length++
+        }
+        return this
+    }
+
 }
