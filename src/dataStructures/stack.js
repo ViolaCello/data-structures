e338a65b0781880e1cf2b900b31ef11dfbfd9c4f
ViolaCello <ViolaCelloNYC@gmail.com>
2021-04-02 11:44:48 -0400

    Stack - .push() onto the beginning of the Stack (list)

--- src/dataStructures/stack.js
+++ src/dataStructures/stack.js
@@ -12,4 +12,17 @@ class Stack {
         this.size = 0
     }
 
+    push(val) { // adds a Node to the BEGINNING of a Stack in constant time - Big O(1)
+        let newNode = new Node(val)
+        if (this.size===0) {
+            this.first = newNode
+            this.last = newNode
+        } else {
+            let oldHead = this.first
+            this.first = newNode
+            newNode.next = oldHead
+        }
+        this.size++
+    }
+
 }
