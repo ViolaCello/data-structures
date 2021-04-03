fecf6804d4acf7cd320de53c8befafdd3680b08b
ViolaCello <ViolaCelloNYC@gmail.com>
2021-04-03 15:29:48 -0400

    Queue Data Structure: #enqueue Ruby

--- src/dataStructures/queue.rb
+++ src/dataStructures/queue.rb
@@ -16,5 +16,16 @@ class Queue
         @size = 0
     end
 
-    
+    def enqueue(val) # add Node to Queue 
+        new_node = Node.new(val)
+        if !@first
+            @first = new_node
+            @last = new_node
+        else 
+            @last.next = new_node
+            @last = new_node
+        end
+        return @size += 1
+    end
+
 end
