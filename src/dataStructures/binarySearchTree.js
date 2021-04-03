ddbdfb519d7a1aebce29cb60aa02aa400ebb52b8
ViolaCello <ViolaCelloNYC@gmail.com>
2021-04-03 16:27:33 -0400

    Binary Search Tree: insert()

--- src/dataStructures/binarySearchTree.js
+++ src/dataStructures/binarySearchTree.js
@@ -9,4 +9,32 @@ class Node {
         constructor() {
             this.root = null
         }
+
+        insert(value) {
+            let newNode = new Node(value)
+            if (!this.root) {
+                this.root = newNode
+                return this
+            }
+           let current = this.root
+           while (current) {
+               if (current.value<value) {
+                   if (!current.right) {
+                       current.right = newNode
+                       current = null
+                   } else {
+                       current = current.right
+                   }
+               } else {
+                   if (!current.left) {
+                       current.left = newNode
+                       current = null
+                   } else {
+                       current = current.left
+                   }
+               }
+           }
+           return this
+        } 
+
     }
