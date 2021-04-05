45a9a6b2716fd963dd7f3cf06c1922d9dfa16efc
ViolaCello <ViolaCelloNYC@gmail.com>
2021-04-05 15:30:00 -0400

    Binary Search Tree: depth first search PreOrder - JavaScript

--- src/dataStructures/binarySearchTree.js
+++ src/dataStructures/binarySearchTree.js
@@ -62,7 +62,7 @@ class Node {
         // breath first search
         // return all the values in a Binary Search Tree, touching all of them only once
 
-        bfSearch() {
+        breathFirstSearch() {
             let queue = []
             let result = []
             queue.push(this.root)
@@ -72,6 +72,18 @@ class Node {
                 if (!!dequeue.left) queue.push(dequeue.left)
                 if (!!dequeue.right) queue.push(dequeue.right)
             }
+            return result
+        }
+
+        depthFirstSearchPreOrder() {
+            const result = []
+            let current = this.root
+            const traverse = (node) => {
+                result.push(node.value)
+                if (!!node.left) traverse(node.left)
+                if (!!node.right) traverse(node.right)
+            }
+            traverse(current)
             return result
         }
 
