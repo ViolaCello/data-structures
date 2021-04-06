0d1f69ef1f8383a6e84b1347d2012ec8d5a5d188
ViolaCello <ViolaCelloNYC@gmail.com>
2021-04-06 16:23:31 -0400

    Binary Heap: MaxBinaryHeap - Ruby #insert - refactored and tested

--- src/dataStructures/MaxBinaryHeap.rb
+++ src/dataStructures/MaxBinaryHeap.rb
@@ -5,10 +5,10 @@ attr_accessor :values
         @values = []
     end
 
-    def insert(value)
+    def insert(value) # works according to VisualAlgo
         @values.push(value)
         index = @values.length - 1
-        while !!index do
+        while index > 0 do
             parent_index = ((index-1)/2).to_i
             break if @values[parent_index] > @values[index]
             @values[index] = @values[parent_index]
