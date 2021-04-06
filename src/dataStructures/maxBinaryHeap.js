5d1b9aba32ec0cfc0752f7f3a976333570728f89
ViolaCello <ViolaCelloNYC@gmail.com>
2021-04-06 15:17:11 -0400

    Binary Heap: MaxBinaryHeap - JavaScript .insert()

---
+++ src/dataStructures/maxBinaryHeap.js
@@ -0,0 +1,23 @@
+class MaxBinaryHeap {
+
+    constructor() {
+    this.values = []
+    }
+
+    insert(value) {
+        this.values.push(value)
+        let index = this.values.length - 1
+        let parentIndex 
+        while (!!index) {
+            parentIndex = Math.floor((index-1)/2)
+            if (this.values[parentIndex]>this.values[index]) {
+                break
+            }
+            this.values[index] = this.values[parentIndex]
+            this.values[parentIndex] = value
+            index = parentIndex
+        }
+        return this
+    }
+
+}
