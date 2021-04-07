c6eeb402337f663be9edaece7e04aaf1e0e7b413
ViolaCello <ViolaCelloNYC@gmail.com>
2021-04-07 14:40:02 -0400

    numberOfSteps

--- src/javaScriptExamples.js
+++ src/javaScriptExamples.js
@@ -62,3 +62,9 @@ const countSmaller = function(nums) {
     result.push(0)
     return result
 }
+
+// Given a non-negative integer num, return the number of steps to reduce it to zero. If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+
+const numberOfSteps = function(num) {
+    
+}
