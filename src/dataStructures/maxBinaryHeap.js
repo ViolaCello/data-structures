a77a669a7332282771aec928b977d91c8f26eac7
ViolaCello <ViolaCelloNYC@gmail.com>
2021-04-13 16:34:09 -0400

    extractMax() draft

--- src/dataStructures/maxBinaryHeap.js
+++ src/dataStructures/maxBinaryHeap.js
@@ -19,14 +19,35 @@ class MaxBinaryHeap {
     }
 
     extractMax() {
+        if (this.values.length===1) {
+            return this.values.pop()
+        }
         let max = this.values[0]
         let tail = this.values.pop()
         this.values[0] = tail
         let index = 0
         let element = this.values[index]
+        // repeat until neither child is greater than the element
+        while(true) {
+            let swapped = null
         let leftIndex = 2 * index + 1
         let rightIndex = 2 * index + 2
-       
+        if (leftIndex > this.values.length) {
+            if(this.values[leftIndex] > element) {
+                swapped = leftIndex
+            }
+        }
+        if (rightIndex > this.values.length) {
+            if( (!swapped && this.values[rightIndex] > element || !!swapped && this.values[rightIndex] > this.values[leftIndex])) {
+                swapped = rightIndex
+            }
+        }
+        if (swapped===null) break
+        this.values[index] = this.values[swapped]
+        this.values[swapped] = element
+        index = swapped
+        }
+        return max
     }
 
 }
