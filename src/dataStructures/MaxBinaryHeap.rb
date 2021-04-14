c46068c70dec3edd3a32f4c0fa7fcfd703bbc48e
ViolaCello <ViolaCelloNYC@gmail.com>
2021-04-14 16:35:36 -0400

    extract_max RUBY

--- src/dataStructures/MaxBinaryHeap.rb
+++ src/dataStructures/MaxBinaryHeap.rb
@@ -18,4 +18,32 @@ attr_accessor :values
         return self
     end
 
+    def extract_max
+        max = @values[0]
+        tail = @values.pop()
+        @values[0] = tail
+        index = 0
+        element = @values[index]
+        while true do 
+            swapped = nil
+            left = 2 * index + 1
+            right = 2 * index + 2
+            if left < @values.length 
+                if @values[left] > element
+                    swapped = left
+                end
+            end
+            if right < @values.length
+                if (!swapped && @values[right] > element) || (!!swapped && @values[right] > @values[left])
+                    swapped = right
+                end
+            end
+            break if !swapped
+            @values[index] = @values[swapped]
+            @values[swapped] = element
+            element = swapped
+        end
+        return max
+    end
+
 end
