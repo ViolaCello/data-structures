89a414fec691eaf4b4531a9609f2020e7a575aa4
ViolaCello <ViolaCelloNYC@gmail.com>
2021-04-15 16:29:19 -0400

    Priority Queue - enqueue correct

--- src/dataStructures/PriorityQueue.js
+++ src/dataStructures/PriorityQueue.js
@@ -12,16 +12,17 @@ class PriorityQueue {
         this.values = []
     }
 
-    enqueue(data, priority){
+    enqueue(data, priority){ // Basically works as a Min Binary Heap
     const newNode = new Node(data, priority)
     this.values.push(newNode)
     let index = this.values.length - 1
     let parentIndex
     while(!!index) {
         parentIndex = Math.floor((index-1)/2)
-        if (this.values[parentIndex].priority>this.values[index].priority) break
-            this.values[index] = this.values[parentIndex]
-            this.values[parentIndex] = this.values[index]
+        if (this.values[parentIndex].priority<=this.values[index].priority) break
+        let temp = this.values[parentIndex]    
+        this.values[parentIndex] = this.values[index]
+            this.values[index] = temp
             index = parentIndex
         }
         return this
