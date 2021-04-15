fceb773681ebaf8a07c9517cd1fad186165d2991
ViolaCello <ViolaCelloNYC@gmail.com>
2021-04-15 16:00:53 -0400

    Priority Queue - enqueue draft

--- src/dataStructures/PriorityQueue.js
+++ src/dataStructures/PriorityQueue.js
@@ -9,9 +9,21 @@ class Node {
 class PriorityQueue {
 
     constructor() {
-        values = []
+        this.values = []
     }
 
-
-    
+    enqueue(data, priority){
+    const newNode = new Node(data, priority)
+    this.values.push(newNode)
+    let index = this.values.length - 1
+    let parentIndex
+    while(!!index) {
+        parentIndex = Math.floor((index-1)/2)
+        if (this.values[parentIndex].priority>this.values[index].priority) break
+            this.values[index] = this.values[parentIndex]
+            this.values[parentIndex] = this.values[index]
+            index = parentIndex
+        }
+        return this
+    }
 }
