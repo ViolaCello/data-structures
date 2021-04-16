884ef08a9a77383e2dd20392caa58620c6836fba
ViolaCello <ViolaCelloNYC@gmail.com>
2021-04-16 10:25:31 -0400

    PriorityQueue class for PriorityQueue and initalizer

--- src/dataStructures/PriorityQueue.rb
+++ src/dataStructures/PriorityQueue.rb
@@ -5,5 +5,15 @@ attr_accessor :data, :priority
         @data = data
         @priority = priority
     end
+end
+
+class PriorityQueue
+attr_accessor :values
+
+    def initialize
+        @values = []
+    end
+
+    
 
 end
