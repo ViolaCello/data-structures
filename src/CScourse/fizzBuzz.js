0f92240f94c1273737dfc5fcae2e435d5320d495
ViolaCello <ViolaCelloNYC@gmail.com>
2021-04-22 15:54:19 -0400

    the classic fizzBizz - correct

--- src/CScourse/fizzBuzz.js
+++ src/CScourse/fizzBuzz.js
@@ -5,7 +5,17 @@
 // if its a multiple of 3 and 5, log 'fizzbuzz'
 
 function fizzBuzz(n) {
-    for (let i = 0; i<n; i++) {
-        
+    for (let i = 1; i<n; i++) {
+        if ( !(i%3) && !(i%5)) {
+            console.log("fizzbuzz")
+        } 
+        else if 
+        ( !(i%5) ) {
+            console.log("buzz")
+        } else if
+        ( !(i%3)) {
+            console.log("fizz")
+        } else 
+        console.log(i)
     }
 }
