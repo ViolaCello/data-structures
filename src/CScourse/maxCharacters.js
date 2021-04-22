13d0def4c839b84bba65bb5e2388f507e6d52bfe
ViolaCello <ViolaCelloNYC@gmail.com>
2021-04-22 15:20:37 -0400

    return the maximum characters in a string - correct

--- src/CScourse/maxCharacters.js
+++ src/CScourse/maxCharacters.js
@@ -4,14 +4,12 @@ function maxChars(str) {
     let chars = {}
     for (let char of str) {
         chars[char] = chars[char]+1 || 1
-        //console.log(chars)
     }
     let result;
     for (let key in chars) {
         console.log(result)
         if (!result) {
-            console.log("I'm in here!", key, chars[key])
-       let result = [key, chars[key]]
+       result = [key, chars[key]]
     } else if 
         (chars[key]>result[1]) { result = [key, chars[key]]
     }
