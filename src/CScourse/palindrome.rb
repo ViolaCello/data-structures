f1b076100799eaeed17825b36ef856790eba17d4
ViolaCello <ViolaCelloNYC@gmail.com>
2021-04-23 12:27:22 -0400

    is_palindrome? RUBY - without using .reverse()

--- src/CScourse/palindrome.rb
+++ src/CScourse/palindrome.rb
@@ -1,3 +1,12 @@
+# fastest way
 def is_palindrome?(word)
     return word==word.reverse
 end
+
+# solution without using .reverse()
+def is_palindrome?(word)
+    reversed = ""
+    word.each_char { |letter| reversed = letter + reversed }
+    return word == reversed
+end
+
