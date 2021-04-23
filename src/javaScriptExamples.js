1f4ae3586e969775c4ee846d97c29eaad9443b9d
ViolaCello <ViolaCelloNYC@gmail.com>
2021-04-23 13:38:27 -0400

    maxIceCream - coding example - added bubbleSort to sort array

--- src/javaScriptExamples.js
+++ src/javaScriptExamples.js
@@ -203,3 +203,32 @@ const countMatches = function(items, ruleKey, ruleValue) {
         }
     return count
 };
+
+// It is a sweltering summer day, and a boy wants to buy some ice cream bars.
+
+// At the store, there are n ice cream bars. You are given an array costs of length n, where costs[i] is the price of the ith ice cream bar in coins. The boy initially has coins coins to spend, and he wants to buy as many ice cream bars as possible. 
+
+// Return the maximum number of ice cream bars the boy can buy with coins coins.
+
+// Note: The boy can buy the ice cream bars in any order.
+
+const maxIceCream = function(costs, coins) {
+    let sorted = bubbleSortOptimized(costs)
+
+}
+
+// helpter to maxIceCream
+function bubbleSortOptimized(arr) {
+    // let temp;
+    for (let i = 0; i<arr.length; i++) {
+        let swapped = false
+        for (let j = 0; j<(arr.length-1-i); j++) {
+            if (arr[j]>arr[j+1]) {
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+                swapped = true
+            }
+        }
+        if (swapped==false) break
+    }
+    return arr
+}
