2690e2b1263c79d7e5a862ff70ff10e2ef0bd975
ViolaCello <ViolaCelloNYC@gmail.com>
2021-04-23 12:18:52 -0400

    reverse a string - RUBY alternate using .each_char

--- src/CScourse/reverse.rb
+++ src/CScourse/reverse.rb
@@ -4,3 +4,10 @@
 def reverse(string)
     return string.reverse
 end
+
+# alternate
+def reverse_string(string)
+    result = ""
+    string.each_char { |char| result = char + result }
+    return result
+end
