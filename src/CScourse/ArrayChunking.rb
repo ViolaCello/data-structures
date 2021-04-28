3c9657923dfcc0b48d4a806cad11f65870bb9169
ViolaCello <ViolaCelloNYC@gmail.com>
2021-04-28 12:33:45 -0400

    array chunking problem - Ruby correct - ALSO, comments about how different .slice works in Ruby vs JavaScript

--- src/CScourse/ArrayChunking.rb
+++ src/CScourse/ArrayChunking.rb
@@ -6,8 +6,11 @@ def array_chunk(array, size)
     result = []
     index = 0
     while array.length > index do
-        result.push(array.splice(index, index + size))
+        result.push(array.slice(index, index + size)) # see below for difference with .slice in Ruuby and JavaScript
         index += size
     end
     return result
 end
+
+# .slice in JavaScript = .slice(indexToBeginAt, indexToEndAt - 1)
+# .slice in Ruby = .slice(index_to_begin_at, number of elements to return)
