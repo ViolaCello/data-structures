73744f8cbd03de33f66e1954b64ad6ebd4feb874
ViolaCello <ViolaCelloNYC@gmail.com>
2021-04-29 16:15:41 -0400

    anagram detector JavaScript - first attempt

--- src/CScourse/anagrams.js
+++ src/CScourse/anagrams.js
@@ -7,5 +7,15 @@ function anagrams(stringA, stringB) {
     if (newA.length != newB.length) return false
     let mapA = {}
     let mapB = {}
-    
+    for (let str of newA) {
+        mapA[str] = mapA[str] + 1 || 1
+    }
+    for (let str of newB) {
+        mapB[str] = mapB[str] + 1 || 1
+    }
+    if (Object.keys(mapA) != Object.keys(mapB)) return false
+    for (let key in newA) {
+        if (mapA[key] != mapB[key]) return false
+    }
+    return true
 }
