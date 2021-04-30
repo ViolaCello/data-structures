f47adc068d8d7b43e5edb8e499a1ce994a0c7d15
ViolaCello <ViolaCelloNYC@gmail.com>
2021-04-30 16:30:42 -0400

    capitalize first letter of each word - works only is all letters are downcase

--- src/CScourse/capitalize.rb
+++ src/CScourse/capitalize.rb
@@ -1,3 +1,13 @@
 def capitalize(str)
-    
+    str[0] = str[0].upcase!
+    i = 1
+    while i<str.length do
+        if str[i].ord == 32 && str[i+1] == str[i+1].downcase
+            str[i+1] = str[i+1].upcase!
+            i += 2
+        else 
+            i += 1
+        end
+    end
+    return str
 end
