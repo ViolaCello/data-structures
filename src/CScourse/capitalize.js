e4e08d3fe1b83ad535beccadd984286f2624899a
ViolaCello <ViolaCelloNYC@gmail.com>
2021-04-30 16:17:48 -0400

    capitalize first letter of each word - JavaScript correct

--- src/CScourse/capitalize.js
+++ src/CScourse/capitalize.js
@@ -1,14 +1,15 @@
 function capitalize(str) {
     let result = ""
     result = str[0].toUpperCase()
-    let i = 0
-    while(let i = 1; i<str.length; i++) {
+    let i = 1
+    while(i<str.length) {
         if (str.charCodeAt(i)==32) {
             result = result + str[i] + str[i+1].toUpperCase()
             i = i + 2
         } else {
-
+            result = result + str[i]
+            i++
         }
     }
-    return str
+    return result
 }
