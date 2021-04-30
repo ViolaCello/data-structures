5f797116696c27250fb2fc99b0ac77ed42a75c78
ViolaCello <ViolaCelloNYC@gmail.com>
2021-04-30 16:05:01 -0400

    anagram detector Ruby - correct

--- src/CScourse/anagrams.rb
+++ src/CScourse/anagrams.rb
@@ -2,19 +2,28 @@
 # downcase.gsub(/[^a-z ]/, "").gsub(/\s+/, "")
 
 def anagram(strA, strB)
-    newA = strA.downcase.gsub(/[^a-z ]/, "").gsub(/\s+/, "")
-    newB = strB.downcase.gsub(/[^a-z ]/, "").gsub(/\s+/, "")
-    mapA = char_map(newA)
-    mapB = char_map(newB)
-    
+    # helper method to map characters of string to a hash
     def char_map(string)
-        map = {}
-        string.each_char do |char|
-            if !!map[char]
-                map[char] = map[char] + 1
-            else 
-                map[char] = 1
-            end
-        end
-    end
-end
+         map_ch = {}
+         string.each_char do |char|
+             if !!map_ch[char]
+                 map_ch[char] = map_ch[char] + 1
+             else 
+                 map_ch[char] = 1
+             end
+         end
+         return map_ch
+       end 
+
+     newA = strA.downcase.gsub(/[^a-z ]/, "").gsub(/\s+/, "")
+     newB = strB.downcase.gsub(/[^a-z ]/, "").gsub(/\s+/, "")
+     mapA = char_map(newA)
+     mapB = char_map(newB)
+
+     return false if mapA.size != mapB.size
+ 
+     mapA.each_key do |key| 
+         return false if mapA[key] != mapB[key] 
+     end
+    return true
+ end
