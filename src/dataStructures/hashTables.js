fa0d587ded55312dad36677bd943735fa1c8559a
ViolaCello <ViolaCelloNYC@gmail.com>
2021-05-11 13:06:05 -0400

    HASH TABLE - basic set-up as a Class

--- src/dataStructures/hashTables.js
+++ src/dataStructures/hashTables.js
@@ -1,7 +1,13 @@
 // creating a hash table from scratch (hashes are called Objects in JavaScript and Dictionaries in Python)
 // The length of the storage location should be a prime number (for some mathematical reason, it reduces "collisions")
 
-function hash(key, arrayLen) {
+class HashTable {
+
+    constructor(size=53) {
+        this.keyMap = new Array(size)
+    }
+
+     hash(key) {
     let total = 0
     const STATIC_PRIME = 31
     // loop through each character of the 'key' or 100 times if there are more than 100 characters in the key
@@ -9,7 +15,9 @@ function hash(key, arrayLen) {
         let char = key[i]
         // in ASCII the lowercase letter 'a' begins at 97
         let value = char.charCodeAt(0) - 96 
-        total = ( total * STATIC_PRIME + value ) % arrayLen
+        total = ( total * STATIC_PRIME + value ) % this.keyMap.length
     }
     return total // returns location of key
+}
+
 }
