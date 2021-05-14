32944092b05c255f86d578518c568c09f20ff345
ViolaCello <ViolaCelloNYC@gmail.com>
2021-05-14 11:05:47 -0400

    HashTable RUBY - #indexing - generates the hash storage location

--- src/dataStructures/HashTable.rb
+++ src/dataStructures/HashTable.rb
@@ -5,19 +5,18 @@ class HashTable
         @hash = Array.new(size)
     end
 
+    def indexing(key)
+        total = 0
+        static_prime = 31
+        i = 0
+        while i < [key.length, 100].min do 
+            char = key[i]
+            value = char.ord - 96
+            total = ( total * static_prime + value ) % @hash.length
+            i += 1
+        end
+        return total.abs()
+    end
 
-
-    # def hash(key, arr_length)
-    #     total = 0
-    #     static_prime = 31
-    #     i = 0
-    #     while i < [key.length, 100].min
-    #         char = key[i]
-    #         value = char.ord - 96
-    #         total = (total * static_prime + value ) % arr_length
-    #         i += 1
-    #     end
-    #     return total
-    # end
-
+ 
 end
