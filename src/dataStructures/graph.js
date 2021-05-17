ea4eb9396eae0d930a9a0841f490bd66ad0f1ad8
ViolaCello <ViolaCelloNYC@gmail.com>
2021-05-17 15:15:02 -0400

    GRAPH - JavaScript removeEdge() -> correct

--- src/dataStructures/graph.js
+++ src/dataStructures/graph.js
@@ -23,26 +23,25 @@ class Graph {
     removeEdge(vertex1, vertex2) {
         let v1 = this.adjacencyList[vertex1]
         let v2 = this.adjacencyList[vertex2]
-        const remove = (x, y) => {
-            for (let i = 0; i<x.length; i++) {
-                console.log(x[i], y)
-                if (x[i]==y) {
-                    console.log(x.splice(i, 1))
-                     return x.splice(i, 1)
+        for (let i = 0; i<v1.length; i++) {
+                if (v1[i]===vertex2) {
+                 v1.splice(i, 1)
                 }
             }
-        }
-        remove(v1, v2)
-        remove(v2, v1)
+        for (let i = 0; i<v2.length; i++) {
+                if (v2[i]===vertex1) {
+                 v2.splice(i, 1)
+                }
+            } 
     }
 
 }
 
-let a = new Graph
-a.addVertex("New York")
-a.addVertex("Chicago")
-a.addVertex("LA")
-a.addVertex("Boston")
-a.addEdge("New York", "LA")
-a.addEdge("New York", "Boston")
-a.addEdge("LA", "Boston")
+// let a = new Graph
+// a.addVertex("New York")
+// a.addVertex("Chicago")
+// a.addVertex("LA")
+// a.addVertex("Boston")
+// a.addEdge("New York", "LA")
+// a.addEdge("New York", "Boston")
+// a.addEdge("LA", "Boston")
