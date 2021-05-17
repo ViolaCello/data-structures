025a9d5cfecfeb4d4b15b6647b20376f561f008b
ViolaCello <ViolaCelloNYC@gmail.com>
2021-05-17 14:56:22 -0400

    GRAPH - JavaScript removeEdge() draft

--- src/dataStructures/graph.js
+++ src/dataStructures/graph.js
@@ -14,9 +14,24 @@ class Graph {
         }
     }
 
+    // if we wanted to make this Graph directional, just add one connection
     addEdge(vertex1, vertex2) {
         this.adjacencyList[vertex1].push(vertex2)
         this.adjacencyList[vertex2].push(vertex1)
+    }
+
+    removeEdge(vertex1, vertex2) {
+        let v1 = this.adjacencyList[vertex1]
+        let v2 = this.adjacencyList[vertex2]
+        const remove = (x, y) => {
+            for (let i = 0; i<x.length; i++) {
+                if (x[i]===y) {
+                     return x.splice(i, 1)
+                }
+            }
+        }
+        remove(vertex1, vertex2)
+        remove(vertex2, vertex1)
     }
 
 }
