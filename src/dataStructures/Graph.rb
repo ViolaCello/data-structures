1554f0c05abff39dfc725f4087bad3ec7b321f12
ViolaCello <ViolaCelloNYC@gmail.com>
2021-05-18 14:12:05 -0400

    GRAPH - Ruby - add_edge

--- src/dataStructures/Graph.rb
+++ src/dataStructures/Graph.rb
@@ -11,4 +11,11 @@ class Graph
         return true
     end
 
+    def add_edge(v1, v2)
+        return false if !@list[v1] || !@list[v2]
+        @list[v1].push(v2)
+        @list[v2].push(v1)
+        return true
+    end
+
 end
