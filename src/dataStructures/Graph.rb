0d5d148e877c8379c68b3b01e5117f34afbea046
ViolaCello <ViolaCelloNYC@gmail.com>
2021-05-18 14:32:26 -0400

    GRAPH - Ruby - remove_edge

--- src/dataStructures/Graph.rb
+++ src/dataStructures/Graph.rb
@@ -18,4 +18,20 @@ class Graph
         return true
     end
 
+    def remove_edge(v1, v2)
+        return false if !@list[v1] || !@list[v2]
+        i = 0
+        while i<@list[v1].length do
+            @list[v1].slice!(i) if @list[v1][i] == v2
+            i += 1
+        end
+        i = 0
+        while i<@list[v2].length do
+            @list[v2].slice!(i) if @list[v2][i] == v1
+            i += 1
+        end
+        # @list.delete_if { |k, v| k == v1 || k == v2} # this is for remove_vertex
+        return true
+    end
+
 end
