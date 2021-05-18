d55f71d35fc73d80e6696b2c0b827c0e25424307
ViolaCello <ViolaCelloNYC@gmail.com>
2021-05-18 14:06:56 -0400

    GRAPH - Ruby - add_vertex

--- src/dataStructures/Graph.rb
+++ src/dataStructures/Graph.rb
@@ -5,6 +5,10 @@ class Graph
         @list = {}
     end
 
-    
+    def add_vertex(vertex)
+        return false if !!@list[vertex]
+        @list[vertex] = []
+        return true
+    end
 
 end
