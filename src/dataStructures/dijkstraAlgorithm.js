9eeb835e848b8a2d7abcda6b2ff76790655bde4f
ViolaCello <ViolaCelloNYC@gmail.com>
2021-05-19 16:19:11 -0400

    Dijkstras Algorithm.  Weighted Graph: addEdge()

--- src/dataStructures/dijkstraAlgorithm.js
+++ src/dataStructures/dijkstraAlgorithm.js
@@ -7,8 +7,13 @@ class Graph {
     }
 
     addVertex(vertex) {
-        if (!this.grapahList[vertex]) this.graphList[vertex] = []
+        if (!this.graphList[vertex]) this.graphList[vertex] = []
     }
 
+    addEdge(v1, v2, weight) {
+        if (!this.graphList[v1] || !this.graphList[v2]) return undefined
+        this.graphList[v1].push({node: v2, weight})
+        this.graphList[v2].push({node: v1, weight})
+    }
 
 }
