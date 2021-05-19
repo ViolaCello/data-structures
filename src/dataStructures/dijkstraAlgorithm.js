45c6c9d5476cb24927edaa07ddd48b06df4c4668
ViolaCello <ViolaCelloNYC@gmail.com>
2021-05-19 16:38:45 -0400

    Dijkstras Algorithm.  PriorityQueue Class for handling weights

--- src/dataStructures/dijkstraAlgorithm.js
+++ src/dataStructures/dijkstraAlgorithm.js
@@ -11,9 +11,26 @@ class Graph {
     }
 
     addEdge(v1, v2, weight) {
-        if (!this.graphList[v1] || !this.graphList[v2]) return undefined
+        if (!this.graphList[v1] || !this.graphList[v2]) return false
         this.graphList[v1].push({node: v2, weight})
         this.graphList[v2].push({node: v1, weight})
+        return true
     }
 
+}
+
+class PriorityQueue {
+    constructor() {
+        this.values = []
+    }
+    enqueue(val, priority) {
+        this.values.push({val, priority})
+        this.sort()
+    }
+    dequeue() {
+        return this.values.shift()
+    }
+    sort() {
+        this.values.sort((a,b) => a.priority - b.priority)
+    }
 }
