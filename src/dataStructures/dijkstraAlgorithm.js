d17494ce1387019548e0260776e6f2fb14375281
ViolaCello <ViolaCelloNYC@gmail.com>
2021-05-20 12:19:29 -0400

    Dijkstras Algorithm.  call PriorityQueue and set it to variable to enqueue

--- src/dataStructures/dijkstraAlgorithm.js
+++ src/dataStructures/dijkstraAlgorithm.js
@@ -21,8 +21,9 @@ class Graph {
         let distances = {}
         Object.keys(this.graphList).forEach(node => distances[node] = Number.POSITIVE_INFINITY)
         distances[start] = 0
-        for (node in this.graphList) {
-            if (distances[node]==Number.POSITIVE_INFINITY) enqueue(node, Number.POSITIVE_INFINITY)
+        let queue = new PriorityQueue
+        for (let node in this.graphList) {
+            if (distances[node]==Number.POSITIVE_INFINITY) queue.enqueue(node, Number.POSITIVE_INFINITY)
         }
         let previous = {}
         Object.keys(this.graphList).forEach(node => previous[node] = null)
@@ -48,3 +49,18 @@ class PriorityQueue {
         this.values.sort((a,b) => a.priority - b.priority)
     }
 }
+
+let a = new Graph
+a.addVertex("A")
+a.addVertex("B")
+a.addVertex("C")
+a.addVertex("D")
+a.addVertex("E")
+a.addVertex("F")
+a.addEdge("A","B",5)
+a.addEdge("A","C", 10)
+a.addEdge("A", "D", 14)
+a.addEdge("B", "C", 3)
+a.addEdge("C", "D", 8)
+a.addEdge("D", "E", 11)
+a.addEdge("E", "F", 9)
