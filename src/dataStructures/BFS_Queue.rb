88557f1c4ece5ec64e33a818c3631b0f8141318c
ViolaCello <ViolaCelloNYC@gmail.com>
2021-05-22 15:31:45 -0400

    BFS with Queue Class for article - added all four necessary classes

--- src/dataStructures/BFS_Queue.rb
+++ src/dataStructures/BFS_Queue.rb
@@ -44,5 +44,64 @@ class Queue
         return 0
        end
     end
+end
+
+class Node
+    attr_accessor :value, :left, :right
+
+    def initialize(value)
+        @value = value
+        @left = nil
+        @right = nil
+    end
+end
+
+class BinarySearchTree
+    attr_accessor :root
+
+    def initialize
+        @root = nil
+    end
+
+    def insert(value)
+        new_node = Node.new(value)
+        if !root
+            @root = new_node
+            return self
+        end
+        current = @root
+        while current do
+            return nil if current.value == value
+            if value > current.value
+                if !current.right
+                    current.right = new_node
+                    break
+                else 
+                    current = current.right
+                end
+            else 
+                if !current.left
+                    current.left = new_node
+                    break
+                else 
+                    current = current.left
+                end
+            end
+        end
+        return self
+    end
+
+    def breath_first_search
+        queue = []
+        result = []
+        queue.push(@root)
+        while queue.length != 0 do
+            dequeued = queue.shift()
+            result.push(dequeued)
+            queue.push(dequeued.left) if !!dequeued.left
+            queue.push(dequeued.right) if !!dequeued.right
+        end
+        return result
+    end
 
 end
