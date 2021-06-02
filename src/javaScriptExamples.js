2c05eda172a64b3ac0c83ba4a1907273cffc291b
ViolaCello <ViolaCelloNYC@gmail.com>
2021-06-02 17:18:27 -0400

    find the Nth prime number - Sieve of Eratosthenes pseudocode - creating loop

--- src/javaScriptExamples.js
+++ src/javaScriptExamples.js
@@ -264,11 +264,13 @@ function primes(n) {
 // same as above, but using Sieve of Eratosthenes
 const eratosthenes = n => {
     // 1. Create a list of consecutive integers from 2 through n: (2, 3, 4, ..., n).
-    
+    const primes = new Array(n).fill(true)
     // 2. Initially, let p equal 2, the smallest prime number.
-
+    let p = 2
     // 3. Enumerate the multiples of p by counting in increments of p from 2p to n, and mark them in the list (these will be 2p, 3p, 4p, ...; the p itself should not be marked).
-
+    for (let i = 0; i<=primes.length; i +=2 ) {
+        primes[i] = false
+    }
     // 4. Find the smallest number in the list greater than p that is not marked. If there was no such number, stop. Otherwise, let p now equal this new number (which is the next prime), and repeat from step 3.
 
     // 5. When the algorithm terminates, the numbers remaining not marked in the list are all the primes below n.
