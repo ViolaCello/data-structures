228f6e12402036433c4f3439869d99bc5785ee85
ViolaCello <ViolaCelloNYC@gmail.com>
2021-06-03 14:17:25 -0400

    find the Nth prime number - Sieve of Eratosthenes: Works correctly

--- src/javaScriptExamples.js
+++ src/javaScriptExamples.js
@@ -265,26 +265,35 @@ function primes(n) {
 const eratosthenes = n => {
     // 1. Create a list of consecutive integers from 2 through n: (2, 3, 4, ..., n).
     const primes = []
-    for (let i = 2; i<=n; i++) {
+    for (let i = 0; i<=n; i++) {
         primes.push(i)
     }
     // 2. Initially, let p equal 2, the smallest prime number.
     let p = 2
     while (p<n) {
+        console.log("Line 274")
     // 3. Enumerate the multiples of p by counting in increments of p from 2p to n, and mark them in the list (these will be 2p, 3p, 4p, ...; the p itself should not be marked).
-        for (let i = p; i<=primes.length; i += p ) {
+        for (let i = p * 2; i<=n; i += p ) {
          primes[i] = false
          }
-         for (let i = p+1; i<primes.length; i++) {
-             if (!!primes[i]) {
-                 let p = primes[i]
-                 console.log("This is p in the loop:", p)
-                 break;
+         console.log("line 278")
+           // 4. Find the smallest number in the list greater than p that is not marked. If there was no such number, stop. Otherwise, let p now equal this new number (which is the next prime), and repeat from step 3.
+         for (let j = p+1; j<=n; j++) {
+             console.log("line 280")
+             if (j===n) {
+        // 5. When the algorithm terminates, the numbers remaining not marked in the list are all the primes below n.   
+                primes.shift()
+                primes.shift()
+                return primes
              }
+            if (!!primes[j]) {
+               p = primes[j]
+                console.log("This is p in the loop:", p)
+                console.log("Line 283")
+                break;
+            }
+            console.log("line 289")
          }
-}
-    // 4. Find the smallest number in the list greater than p that is not marked. If there was no such number, stop. Otherwise, let p now equal this new number (which is the next prime), and repeat from step 3.
-return primes
-    // 5. When the algorithm terminates, the numbers remaining not marked in the list are all the primes below n.
-
+         console.log("line 291")
+    }
 }
