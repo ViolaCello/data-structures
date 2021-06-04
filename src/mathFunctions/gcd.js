bc86bed8b1b3ebe1f3bc814d3fe27ec7ae6a77ef
ViolaCello <ViolaCelloNYC@gmail.com>
2021-06-04 13:56:24 -0400

    formula for Euler's Totient function that returns maximum N where n/totient - using multiple's formula to speed it up

--- src/mathFunctions/gcd.js
+++ src/mathFunctions/gcd.js
@@ -30,23 +30,14 @@ const totient = n => {
 
 // find highest value of n <= 1,000,000 for which n/titient is maximum
 
-const maxTotient = n => {
+const maxTotientOldWay = n => {
     let max = [0,0] // n is on left, Max avgTotient is on right 
     let currentTotient;
     let avgTotient;
-    let primes = eratosthenes(Math.sqrt(n))
     for (let i = 2; i<=n; i++) {
-        if (i>Math.sqrt(n)) {
-            for (let j = 0; j<=primes.length; j++) {
-                if (i%primes[j]===0) {
-                    currentTotient = primes[j] - 1
-                    break;
-                }
-            }
-        } else if 
-             (primes.includes(i)) {
+        if (!!isPrime(i)) {
             currentTotient = i - 1
-        } else   {
+        } else {
         currentTotient = totient(i)
         }
         avgTotient = i / currentTotient
@@ -58,38 +49,51 @@ const maxTotient = n => {
     return max 
 }
 
-const eratosthenes = n => {
-    // 1. Create a list of consecutive integers from 2 through n: (2, 3, 4, ..., n).
-    const primes = []
-    for (let i = 0; i<=n; i++) {
-        primes.push(i)
-    }
-    // 2. Initially, let p equal 2, the smallest prime number.
-    let p = 2
-    while (p<n) {
-    // 3. Enumerate the multiples of p by counting in increments of p from 2p to n, and mark them in the list (these will be 2p, 3p, 4p, ...; the p itself should not be marked).
-        for (let i = p * 2; i<=n; i += p ) {
-         primes[i] = false
-         }
-           // 4. Find the smallest number in the list greater than p that is not marked. If there was no such number, stop. Otherwise, let p now equal this new number (which is the next prime), and repeat from step 3.
-         for (let j = p+1; j<=n; j++) {
-             if (j===n) {
-        // 5. When the algorithm terminates, the numbers remaining not marked in the list are all the primes below n.   
-                primes.shift()
-                primes.shift()
-                let k = 0
-                while (k<primes.length) {
-                    if (!primes[k]) {
-                        primes.splice(k,1)
-                         k--}
-                         k++
+// Phi(A*B) = Phi(A) * PHI(B)
+// Therefore, there should be a way to store all the answers to these, then retrieve them...
+// So, let's try like this:
+const maxTotient = n => {
+    let tots = [null, null]
+    let max = [0,0] // n is on left, Max avgTotient is on right 
+    let currentTotient;
+    let avgTotient;
+    for(let i = 2; i<=n; i++) {
+        if (!!isPrime(i)) {
+            tots[i] = i - 1
+        } else if 
+              (tots.length>5 && i%2===0) {
+                tots[i] = tots[2] * tots[i/2]
+            } else if 
+                (tots.length>5 && i%3===0) {
+                    tots[i] = tots[3] * tots[i/3]
+                } else {
+                    currentTotient = totient(i)
                 }
-                return primes
-             }
-            if (!!primes[j]) {
-               p = primes[j]
-                break;
+                avgTotient = i / currentTotient
+                if (avgTotient >= max[1]) {
+                    max[0] = i
+                    max[1] = avgTotient
+                }
             }
-         }
+            return max
     }
-}
+
+
+
+
+// from Wiki
+function isPrime(num) {
+    if (num <= 3) return num > 1;
+    
+    if ((num % 2 === 0) || (num % 3 === 0)) return false;
+    
+    let count = 5;
+    
+    while (Math.pow(count, 2) <= num) {
+      if (num % count === 0 || num % (count + 2) === 0) return false;
+      
+      count += 6;
+    }
+    
+    return true;
+  }
