cb9384a7b3d9009b62f26a6dfafac8b530a262b2
ViolaCello <ViolaCelloNYC@gmail.com>
2021-06-04 12:52:22 -0400

    formula for Euler's Totient function that returns maximum N where n/totient - making it faster

--- src/mathFunctions/gcd.js
+++ src/mathFunctions/gcd.js
@@ -19,6 +19,9 @@ const totient = n => {
     let relativelyPrime = []
     let rp;
     for (let i = 1; i<n; i++) {
+        if (i>2 && n%i===0) { continue }
+        if (i>2 && n%2===0 && i%2===0) { continue }
+        if (i>3 && n%3===0 && i%3===0) { continue }
         rp = gcd(i,n)
         if (rp==1) relativelyPrime.push(i)
     }
@@ -31,8 +34,21 @@ const maxTotient = n => {
     let max = [0,0] // n is on left, Max avgTotient is on right 
     let currentTotient;
     let avgTotient;
+    let primes = eratosthenes(Math.sqrt(n))
     for (let i = 2; i<=n; i++) {
+        if (i>Math.sqrt(n)) {
+            for (let j = 0; j<=primes.length; j++) {
+                if (i%primes[j]===0) {
+                    currentTotient = primes[j] - 1
+                    break;
+                }
+            }
+        } else if 
+             (primes.includes(i)) {
+            currentTotient = i - 1
+        } else   {
         currentTotient = totient(i)
+        }
         avgTotient = i / currentTotient
         if (avgTotient >= max[1]) {
             max[0] = i
@@ -40,4 +56,40 @@ const maxTotient = n => {
         }
     }
     return max 
+}
+
+const eratosthenes = n => {
+    // 1. Create a list of consecutive integers from 2 through n: (2, 3, 4, ..., n).
+    const primes = []
+    for (let i = 0; i<=n; i++) {
+        primes.push(i)
+    }
+    // 2. Initially, let p equal 2, the smallest prime number.
+    let p = 2
+    while (p<n) {
+    // 3. Enumerate the multiples of p by counting in increments of p from 2p to n, and mark them in the list (these will be 2p, 3p, 4p, ...; the p itself should not be marked).
+        for (let i = p * 2; i<=n; i += p ) {
+         primes[i] = false
+         }
+           // 4. Find the smallest number in the list greater than p that is not marked. If there was no such number, stop. Otherwise, let p now equal this new number (which is the next prime), and repeat from step 3.
+         for (let j = p+1; j<=n; j++) {
+             if (j===n) {
+        // 5. When the algorithm terminates, the numbers remaining not marked in the list are all the primes below n.   
+                primes.shift()
+                primes.shift()
+                let k = 0
+                while (k<primes.length) {
+                    if (!primes[k]) {
+                        primes.splice(k,1)
+                         k--}
+                         k++
+                }
+                return primes
+             }
+            if (!!primes[j]) {
+               p = primes[j]
+                break;
+            }
+         }
+    }
 }
