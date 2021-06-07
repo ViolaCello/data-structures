baf428bab5d9622249d83f629dc32a80a40b65ce
ViolaCello <ViolaCelloNYC@gmail.com>
2021-06-07 13:46:06 -0400

    Find the largest Prime Factor for N - better version

--- src/mathFunctions/primeFactorization.js
+++ src/mathFunctions/primeFactorization.js
@@ -1,7 +1,7 @@
 // solve for the largest prime factor
 
 const isPrime = num => {
-    if (num <= 3) return num > 1;
+   // if (num <= 3) return num > 1;
     
     if ((num % 2 === 0) || (num % 3 === 0)) return false;
     
@@ -9,7 +9,6 @@ const isPrime = num => {
     
     while (Math.pow(count, 2) <= num) {
       if (num % count === 0 || num % (count + 2) === 0) return false;
-      
       count += 6;
     }
     
@@ -18,10 +17,11 @@ const isPrime = num => {
 
   const largestPrimeFactor = n => {
         if (!!isPrime(n)) return n
-        for (let i = n; i>(n/2); i--) {
-            console.log(i)
+        for (let i = Math.ceil(n/2); i>1; i--) {
             // if (i % 2 === 0 || i % 3 === 0 || i % 5 ===0 || i % 7 === 0) { continue };
-           if ( n & i === 0 && !!isPrime(i)) return i
+            if (!!isPrime(i)) {
+                if ( n % i === 0) return i
+           } 
         }
         return false
   }
