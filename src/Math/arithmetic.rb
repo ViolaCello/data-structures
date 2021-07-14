ac274db1145d1ae73460b824a1afa044a9058d9e
ViolaCello <ViolaCelloNYC@gmail.com>
2021-07-14 16:46:35 -0400

    division

--- src/Math/arithmetic.rb
+++ src/Math/arithmetic.rb
@@ -1,5 +1,5 @@
 # division only using addition
-# return an object with quotent and remainder if not 0, 1, or false
+# return an object with quotient and remainder if not 0, 1, or false
 def divide(dividend, divisor)
     return false if divisor==0
     return 0 if dividend==0
@@ -10,7 +10,7 @@ def divide(dividend, divisor)
     div = dividend
     while true do 
       count += 1
-      return {"quotent":count, "remainder":(div-divisor)} if (div - divisor) < divisor
+      return {"quotient":count, "remainder":(div-divisor)} if (div - divisor) < divisor
       div = div - divisor
     end
 end
